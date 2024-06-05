af33c007cbe966e5b4eb0fe0d5134a6fa767392f
yusuf08 <khusseinyussuf@usf.edu>
2024-06-04 20:53:02 -0400

    Js and Html added

---
+++ Ball.js
@@ -0,0 +1,41 @@
+class Ball {
+    constructor(x, y, radius, dx, dy, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.dx = dx;
+        this.dy = dy;
+        this.color = color;
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    update(canvas) {
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+}
+
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+const ball = new Ball(200, 160, 20, 2, 2, 'red');
+
+setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ball.update(canvas);
+    ball.draw(ctx);
+}, 10); 
